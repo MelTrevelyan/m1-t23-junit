4033c78d277abacadf16b728402c16c5feb03a6b
Daria Tolstonogova <dtolstonogova@yandex.ru>
2023-01-11 16:50:04 +0700

    feat: add tests for ReversePolishNotationCalculator

--- test/notation/ReversePolishNotationCalculatorTest.java
+++ test/notation/ReversePolishNotationCalculatorTest.java
@@ -1,11 +1,35 @@
 package notation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ReversePolishNotationCalculatorTest {
 
+    ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+
     @Test
     public void shouldCalculateAddition() {
+        Assertions.assertEquals(6, calculator.calculatePolishNotation("1 2 3 + +"));
+    }
 
+    @Test
+    public void shouldCalculateSubtraction() {
+        Assertions.assertEquals( 2, calculator.calculatePolishNotation("3 2 1 - -"));
+    }
+
+    @Test
+    public void shouldCalculateMultiplication() {
+        Assertions.assertEquals(6, calculator.calculatePolishNotation("1 2 3 * *"));
+    }
+
+    @Test
+    public void shouldProperlyHandleNegativeNumbers() {
+        Assertions.assertEquals(-5, calculator.calculatePolishNotation("-1 -2 -3 -4 + - *"));
+
+    }
+
+    @Test
+    public void shouldProperlyHandleBlanks() {
+        Assertions.assertEquals(7, calculator.calculatePolishNotation("4 3 2 1    - * +"));
     }
 }
